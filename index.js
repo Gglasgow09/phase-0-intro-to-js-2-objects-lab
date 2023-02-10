1ceba6543d4710f6a47af5d893c0c2ca8addf166
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-02-10 17:44:18 -0500

    completedlab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Write your solution in this file! 
+
+const employee = {
+    name: 'Gabby'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee['streetAddress'] = '11 Broadway'
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    const streetAddress = 'delete employee.name'
+    employee ['streetAddress'] = '12 Broadway'
+    return employee
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    const newEmployee = {newEmployee}
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee.name
+    return employee
+}
